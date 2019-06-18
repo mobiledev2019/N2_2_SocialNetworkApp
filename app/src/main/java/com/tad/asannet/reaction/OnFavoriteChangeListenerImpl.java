9fc60c974ff3152e2b922b59a29f104d3e092412
bac_vu <vuvan.chanqua@gmail.com>
2019-06-18 20:23:46 +0700

    Update code + report

--- app/src/main/java/com/tad/asannet/reaction/OnFavoriteChangeListenerImpl.java
+++
@@ -1,8 +0,0 @@
-package com.tad.asannet.reaction;
-
-class OnFavoriteChangeListenerImpl implements ReactButton.OnFavoriteChangeListener {
-    @Override
-    public void onFavoriteChanged(ReactButton buttonView, boolean favorite) {
-
-    }
-}
